9f49077493cd4c98ef8f886f58a130a5f9b00b43
Priscila Moreno <priscila.moreno.wow@gmail.com>
2017-12-13 17:35:20 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,16 +1,19 @@
+var lyric = "maybe";
+
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
+thisIsCrazy();
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
